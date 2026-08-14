7f6782cec962eee22b168db3f1e8ec396a7de165
Shaikh-Aabid <shaikhaabidhussain@gmail.com>
2026-08-15 01:28:06 +0530

    refactor: migrate project architecture from React to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://shaikh-aabid.github.io',
+  base: '/portfollio',
+  outDir: './dist',
+  publicDir: './public',
+});
